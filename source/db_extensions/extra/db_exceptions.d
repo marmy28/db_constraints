744ac240f7685e2ce58dabd48239223de1f58c62
Matthew Armbruster <marmbru28@gmail.com>
2015-08-27 16:42:10 -0500

    adding in UniqueConstraintException. Also making the exception more advanced.

--- source/db_extensions/extra/db_exceptions.d
+++ source/db_extensions/extra/db_exceptions.d
@@ -9,9 +9,29 @@ class PrimaryKeyException : Exception
 /**
 Params:
     msg = the message thrown with the primary key violation.
+    next = references the exception that was being handled when
+    this one was generated
  */
-    this(string msg)
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null)
     {
-        super("Primary key violation. " ~ msg);
+        super("Primary key violation. " ~ msg, file, line, next);
+    }
+}
+
+/**
+Inherits from Exception. This is thrown whenever
+there is a unique constraint violation.
+ */
+class UniqueConstraintException : Exception
+{
+/**
+Params:
+    msg = the message thrown with the unique constraint violation.
+    next = references the exception that was being handled when
+    this one was generated
+ */
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null)
+    {
+        super("Unique constraint violation. " ~ msg, file, line, next);
     }
 }
